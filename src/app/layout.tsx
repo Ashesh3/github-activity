db77cf210649095080066568c97122cd306aeedb
Ashesh3 <3626859+Ashesh3@users.noreply.github.com>
2023-07-11 18:00:31 +0530

    Fix FOUC issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
+"use client";
+
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import { useState } from "react";
+import { createCache, extractStyle, StyleProvider } from "@ant-design/cssinjs";
+import { useServerInsertedHTML } from "next/navigation";
 
 export const metadata: Metadata = {
   title: 'GitHub Activity Report',
@@ -12,11 +14,34 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head />
+      <body>
+        <StyleProviderLayout>{children}</StyleProviderLayout>
+      </body>
     </html>
-  )
+  );
+}
+
+function StyleProviderLayout({ children }: { children: React.ReactNode }) {
+  const [cache] = useState(() => createCache());
+
+  const render = <>{children}</>;
+
+  useServerInsertedHTML(() => {
+    return <script
+        dangerouslySetInnerHTML={{
+          __html: `</script>${extractStyle(cache)}<script>`,
+        }}
+      />;
+  });
+
+  if (typeof window !== "undefined") {
+    return render;
+  }
+
+  return <StyleProvider cache={cache}>{render}</StyleProvider>;
 }
